1e7baf6edf1f939a7e2da6a7adfcc5778b217eac
Yongmin Kim <yongmin86k@gmail.com>
2019-09-24 21:23:30 -0700

    Increase the size of uploading files

--- inc/extras.php
+++ inc/extras.php
@@ -11,6 +11,11 @@
  * @param array $classes Classes for the body element.
  * @return array
  */
+
+@ini_set( 'upload_max_size' , '64M' );
+@ini_set( 'post_max_size', '64M');
+@ini_set( 'max_execution_time', '300' );
+
 function ymk_dev_body_classes( $classes ) {
 	// Adds a class of group-blog to blogs with more than 1 published author.
 	if ( is_multi_author() ) {
