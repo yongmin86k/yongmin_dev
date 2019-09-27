a05164ebb3d71f75bba395e6ed44c61ee443c82c
Yongmin Kim <yongmin86k@gmail.com>
2019-09-26 22:12:28 -0700

    Create elements and add styles of work-tags

--- js/filter-front-works.js
+++ js/filter-front-works.js
@@ -11,18 +11,14 @@
 
         // Custom styling for input[t=radio] -> $works
             activateFilterStyles($obj);
-
             $obj.on('click', 'label', function(){
                 activateFilterStyles($obj, $(this));
             });
 
-
-
+        // Fetch localized rest wp-api
             fetchWordPressData();
-
             $obj.on('click', 'input', function(){
                 fetchWordPressData( $(this) );
-                // console.log( $(this) );
              });
 
         }
@@ -87,10 +83,17 @@
         }).done(function(result){
             const arr_data = result;
 
+            // Sort taxonomies in ASC order
+            arr_data.forEach( function(data) {
+               data.work_taxonomies.sort((a, b) => {
+                    return a - b
+               });
+            });
+
+
             arr_data.forEach( function(data, index){
                 let date_start, date_end, date_parsed, title,
-                    url, roles, tag__main, tag__sub;
-
+                    url, roles;
 
                 date_start = data.cmb2.work_details.date_start.slice(0,7).replace('-', '. ');
                 date_end = data.cmb2.work_details.date_end.slice(0,7).replace('-', '. ');
@@ -99,21 +102,62 @@
 
                 url = data.cmb2.work_details.work_url;
 
-                console.log( data.cmb2 );
-
+                // Create general info of each latest work
                 $('#contents-front-page-works').append(`
                     <article id="ymk-works-${index}" class="article-works">
                         <div class="article-meta">
                             <p class="work-date">${date_parsed}</p>
                             <h3 class="work-title">${title}</h3>
                             <a class="work-link" href="${url}">
-                                Read more <i class="fas fa-long-arrow-alt-right"></i>
+                                Read more
                             </a>
                         </div>
-                        
+                        <div class="tag-meta"></div>
                     </article>
-               `);
-            });
+               `); // end $('#contents-front-page-works').append()
+
+                // Create tags of each latest work
+                if (data.work_taxonomies.length > 0) {
+
+                    data.work_taxonomies.forEach(function (value) {
+                        let checkValue, $thisID, $thisParentID, $thisLink, $thisName;
+
+                        checkValue = workTaxonomies.find(function(workTaxonomy){
+                            return workTaxonomy.id === value;
+                        });
+
+                        $thisID = checkValue.id;
+                        $thisLink = checkValue.link;
+                        $thisName = checkValue.name;
+
+                        if ( checkValue.parent === 0 ){
+                        // Create tag container and add the first tag
+                            $(`#ymk-works-${index}`).find('.tag-meta').append(`
+                                <div class='tag-container parent-tag-id-${$thisID}'>
+                                    <p class="tag-element child-tag-id-${$thisID}">
+                                        <a class="tag-link" href="${$thisLink}">
+                                            ${$thisName}
+                                        </a>
+                                    </p>
+                                </div>
+                            `);
+                        } else {
+                        // Add a tag relevant to its parent tag
+                            $thisParentID = checkValue.parent;
+                            $(`#ymk-works-${index}`).find(`.parent-tag-id-${$thisParentID}`).append(`
+                                <p class="tag-element child-tag-id-${$thisID}">
+                                    <a class="tag-link" href="${$thisLink}">
+                                        ${$thisName}
+                                    </a>
+                                </p>
+                            `);
+                        }
+
+                    });
+
+                }
+
+            }); // end arr_data.forEach()
 
         });
 
